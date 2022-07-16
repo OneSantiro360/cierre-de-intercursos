01044d6e7749d05443298f7b8ba18ff3add6a5c0
OneSantiro360 <johndoe@example.com>
2022-07-16 10:01:19 -0500

    commit

---
+++ js/responsive.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-left").classList.toggle("show");
+});
+
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".menu-1").classList.toggle("show");
+});
